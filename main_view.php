59f275b7871b5e31b9e627399e7e43743645eb3c
lrasmus7 <captrazz@gmail.com>
2014-10-07 10:23:58 -0600

    Merge branch 'master' of https://github.com/jordanyoungberg/designsuu
    Conflicts:
    	admin_login.html

---
+++ main_view.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<div id="head"><h1>SUU</h1>
+	<h3>Web 1</h3>
+    	<ul>
+        	<li>Fall 2014</li>
+            <li>Spring 2014</li>
+            <li>Fall 2013</li>
+            <li>Spring 2013</li>
+        </ul>
+    <h3>Web 2</h3>
+    	<ul>
+        	<li>Fall 2014</li>
+            <li>Spring 2014</li>
+            <li>Fall 2013</li>
+            <li>Spring 2013</li>
+        </ul>
+    <h3>Web 3</h3>
+    	<ul>
+        	<li>Fall 2014</li>
+            <li>Spring 2014</li>
+            <li>Fall 2013</li>
+            <li>Spring 2013</li>
+        </ul></div>
+        
+    <div id="title"><h1>Class Number / Name</h1>
+    <h1>Instructor</h1></div>
+    
+    <div id="content">
+    <h3>Project 1</h3>
+    <a href:"#"><img src="#"/></a>
+     <p>Student Name</p>
+    <a href:"#"><img src="#"/></a>
+     <p>Student Name</p>
+    <a href:"#"><img src="#"/></a>
+     <p>Student Name</p>
+    
+      <h3>Project 2</h3>
+    <a href:"#"><img src="#"/></a>
+     <p>Student Name</p>
+    <a href:"#"><img src="#"/></a>
+     <p>Student Name</p>
+    <a href:"#"><img src="#"/></a>
+     <p>Student Name</p>
+    
+     <h3>Project 3</h3>
+    <a href:"#"><img src="#"/></a>
+     <p>Student Name</p>
+    <a href:"#"><img src="#"/></a>
+     <p>Student Name</p>
+    <a href:"#"><img src="#"/></a>
+     <p>Student Name</p></div>
+     
+   <footer>&copy;2014 Southern Utah Univeristy Graphic Design</footer>    
+</body>
+</html>
